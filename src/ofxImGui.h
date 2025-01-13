bc8dab299a69ad9b2723e4d449b0dc201a1513b9
Bruno Herfst <bruno.herfst@gmail.com>
2025-01-13 11:36:30 +1100

    Update ofxImGui.h

--- src/ofxImGui.h
+++ src/ofxImGui.h
@@ -3,6 +3,9 @@
 // Todo: try to detect the best settings from the OF compiler flags
 
 #include "ofxImGuiConstants.h"
+
+#define IMGUI_DEFINE_MATH_OPERATORS
+
 #include "imgui.h"
 #include "Gui.h"
-//#include "ImHelpers.h" // Note: not included anymore by default, include it manually if you need it !
+#include "ImHelpers.h" // Note: not included anymore by default, include it manually if you need it !
